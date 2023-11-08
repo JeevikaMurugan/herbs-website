d04007c477ae4e015d73c2b48306e7bca9da074d
jeevikoushi <jeevikalm.21it@kongu.edu>
2023-11-08 10:08:00 +0530

    first commit

---
+++ src/homeproduct.js
@@ -0,0 +1,64 @@
+
+const Homeproduct = 
+[
+    {
+        id:1,
+        Title:"It is a succulent plant known for its soothing and healing properties",
+        Cat: 'Alovera',
+        Price: '100',
+        Img: './wimg/alo.jpg'
+    },
+    {
+        id:2,
+        Title:"Traditional Medicinals Organic Chamomile Herbal Tea",
+        Cat: 'Chamomile',
+        Price: '168',
+        Img: './wimg/chamo.jpg'
+    },
+    {
+        id:3,
+        Title:"The Republic of Tea Ginger Peach Green Tea",
+        Cat: 'Ginger',
+        Price: '50',
+        Img: './wimg/gin.jpeg'
+    },
+    {
+        id:4,
+        Title:"Help toprevent or reduce the severity of colds and other respiratory infections. ",
+        Cat: 'Echinacea',
+        Price: '1049',
+        Img: './wimg/ech.jpeg'
+    },
+    
+    {
+        id:5,
+        Title:"It is known for its delightful and relaxing fragrance.",
+        Cat: 'Lavendar',
+        Price: '49',
+        Img: './wimg/lav.jpeg'
+    
+    },
+    {
+        id:6,
+        Title:" known for its potential to support liver health. .",
+        Cat: 'Milk Thistle',
+        Price: '156',
+        Img: './wimg/milk.jpeg'
+    
+    },
+    {
+        id:7,
+        Title:" Help to promote mental alertness and clarity..",
+        Cat: 'Pepper Mint',
+        Price: '2098',
+        Img: './wimg/pepper.jpeg'
+    },
+    {
+        id:8,
+        Title:" Help to reduce inflammation in the body.",
+        Cat: 'Red Clover',
+        Price: '386',
+        Img: './wimg/red.jpeg'
+    },
+]
+export default Homeproduct
---
+++ src/productdetail.js
@@ -0,0 +1,92 @@
+
+const Productdetail =
+[
+    {
+        id:1,
+        Title:"Natural and nourishing herbal shampoo for healthier, revitalized hair",
+        Cat: 'Shampoo',
+        Price: '100',
+        Img: './wimg/sh5.jpeg'
+    },
+    {
+        id:2,
+        Title:"Transform your hair with our natural herbal oil.",
+        Cat: 'Hair oil',
+        Price: '168',
+        Img: './wimg/o1.jpeg'
+    },
+    {
+        id:3,
+        Title: "Experience the purity of nature with our herbal-infused soap for a rejuvenating bath.",
+        Cat: 'Soap',
+        Price: '50',
+        Img: './wimg/s1.jpeg'
+    },
+    {
+        id:4,
+        Title:"Effective herbal medicine for holistic health.",
+        Cat: 'Medicine',
+        Price: '1049',
+        Img: './wimg/m2.jpeg'
+    },
+    
+    {
+        id:5,
+        Title:"Gentle herbal shampoo for a soothing and invigorating hair care experience",
+        Cat: 'Shampoo',
+        Price: '49',
+        Img: './wimg/sh8.jpeg'
+    
+    },
+    {
+        id:6,
+        Title:"Glow naturally with our herbal face cream, crafted to enhance your skin's radiance.",
+        Cat: 'Face Cream',
+        Price: '156',
+        Img: './wimg/f1.jpeg'
+    
+    },
+    {
+        id:7,
+        Title: "Nature's remedy in a potent herbal medicine.",
+        Cat: 'Medicine',
+        Price: '2098',
+        Img: './wimg/m3.jpeg'
+    },
+    {
+        id:8,
+        Title:"Nourish and revitalize hair with our herbal oil.",
+        Cat: 'Hair oil',
+        Price: '386',
+        Img: './wimg/o2.jpeg'
+    },
+    {
+        id:9,
+        Title:"Herbal healing in every dose of our medicine.",
+        Cat: 'Medicine',
+        Price: '693',
+        Img: './wimg/m1.jpeg'
+    },
+    {
+        id:10,
+        Title:"Enhance your skin's radiance with our herbal face cream.",
+        Cat: 'Face Cream',
+        Price: '5036',
+        Img: './wimg/f2.jpeg'
+    },
+    {
+        id:11,
+        Title:"Pamper your skin with our all-natural herbal soap for a refreshing cleanse.",
+        Cat: 'Soap',
+        Price: '198',
+        Img: './wimg/s2.jpeg'
+    },
+    {
+        id:12,
+        Title:"Nourish your locks with our herbal hair oil for strong, shiny, and vibrant hair.",
+        Cat: 'Hair oil',
+        Price: '793',
+        Img: './wimg/o3.jpeg'
+    },
+]
+export default Productdetail;
